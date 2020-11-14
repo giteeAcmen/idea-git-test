a0556b697c77a2cec8b2def654cae765659941d3
SaturdayGitHub <1622967526@qq.com>
2020-11-14 19:45:35 +0800

    提交一个类

---
+++ src/main/java/cn/lyq/manage/controller/PersonService.java
@@ -0,0 +1,13 @@
+package cn.lyq.manage.controller;
+
+/**
+ * @ClassName PersonService
+ * @Date 2020/11/14 0014 19:40
+ **/
+public class PersonService {
+
+    public static void main(String[] args) {
+
+        System.out.println("给git添加一个类!");
+    }
+}
