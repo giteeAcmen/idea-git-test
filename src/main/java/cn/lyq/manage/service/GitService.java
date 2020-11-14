58292b0a2eb3dfa161d177ac614edc013c57ccc6
SaturdayGitHub <1622967526@qq.com>
2020-11-14 20:46:41 +0800

    添加了一个service类

---
+++ src/main/java/cn/lyq/manage/service/GitService.java
@@ -0,0 +1,12 @@
+package cn.lyq.manage.service;
+
+/**
+ * @ClassName GitService
+ * @Date 2020/11/14 0014 20:44
+ **/
+public class GitService {
+
+    public static void main(String[] args) {
+
+    }
+}
